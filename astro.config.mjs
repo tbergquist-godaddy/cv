dd517d4feaa282741969b7fdc243713c207eca35
Trond Bergquist <tbergquist@godaddy.com>
2023-03-04 13:47:46 +0100

    add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   integrations: [react()],
+  output: "server",
+  adapter: vercel()
 });
