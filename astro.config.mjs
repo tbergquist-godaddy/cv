86f3aa0ea676bbe8dcc924323600f32d43e8478b
Trond Bergquist <tbergquist@godaddy.com>
2023-03-05 21:04:56 +0100

    remove vercel server setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   integrations: [react()],
-  output: "server",
-  adapter: vercel()
 });
